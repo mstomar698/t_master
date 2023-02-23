0ae5801398617dcd2ff695d4d98eee8422f34a28
mstomar698 <tomarm698@gmail.com>
2023-02-24 01:12:03 +0530

    refactoring files /src/utils

--- src/utils/display.ts
+++ src/utils/display.ts
@@ -2,19 +2,25 @@
 /* eslint-disable no-console */
 import chalk from 'chalk';
 
-const info = (...message: string[]) => {
+interface Display {
+  info: (...message: string[]) => void;
+  warn: (...message: string[]) => void;
+  error: (...message: string[]) => void;
+  log: typeof console.log;
+}
+
+const info: Display['info'] = (...message: string[]) => {
   console.info(chalk.cyanBright.bold(' INFO '), ...message);
 };
 
-const warn = (...message: string[]) => {
+const warn: Display['warn'] = (...message: string[]) => {
   console.warn(chalk.bgYellowBright.bold(' WARN '), ...message);
 };
 
-const error = (...message: string[]) => {
+const error: Display['error'] = (...message: string[]) => {
   console.error(chalk.bgRedBright.bold(' ERROR '), ...message);
 };
 
-const log = console.log;
+const log: Display['log'] = console.log;
 
-export const display = { info, warn, error, log };
-
+export const display: Display = { info, warn, error, log };
