df3bdd218dcc9ef59c295ff1c515bb091bb9c3a1
mstomar698 <tomarm698@gmail.com>
2023-02-23 22:58:35 +0530

    setting up vitest testing env

---
+++ config/vitest.ts
@@ -0,0 +1,17 @@
+// config/vitest.ts
+// The vitest configuration file.
+
+import { env } from 'node:process';
+import { defineConfig } from 'vitest/config';
+
+// Make sure the output of the CLI is in color, so that it matches the
+// snapshots.
+env.FORCE_COLOR = 2;
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  test: {
+    // Collect coverage using C8.
+    coverage: { enabled: true },
+  },
+});
